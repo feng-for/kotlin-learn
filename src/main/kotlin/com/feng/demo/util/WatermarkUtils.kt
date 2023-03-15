095ccfde3de254f48b84b59168751b3872a339fe
fengzhiwei <1576939575@qq.com>
2023-03-15 22:21:40 +0800

    todo: 图片去水印

--- src/main/kotlin/com/feng/demo/util/WatermarkUtils.kt
+++ src/main/kotlin/com/feng/demo/util/WatermarkUtils.kt
@@ -2,16 +2,23 @@ package com.feng.demo.util
 
 import nu.pattern.OpenCV
 import org.opencv.core.*
+import org.opencv.highgui.HighGui
 import org.opencv.highgui.HighGui.imshow
 import org.opencv.highgui.HighGui.waitKey
 import org.opencv.imgcodecs.Imgcodecs
 import org.opencv.imgcodecs.Imgcodecs.imwrite
 import org.opencv.imgproc.Imgproc
 import org.opencv.imgproc.Imgproc.*
+import org.opencv.photo.Photo
 
 
 object WatermarkUtils {
-    fun loadImage(imagePath: String?): Mat? {
+
+    private const val path = "/Users/wei/Downloads/8629.jpg_wh300.jpg"
+
+    private const val d = "/Users/wei/Downloads"
+
+    private fun loadImage(imagePath: String?): Mat? {
         return Imgcodecs.imread(imagePath)
     }
     fun saveImage(imageMatrix: Mat?, targetPath: String?) {
@@ -19,26 +26,26 @@ object WatermarkUtils {
     }
     fun testOpenCV() {
         // 读取图像
-        val image = loadImage("C:\\Users\\Fengzhiwei\\Pictures\\Screenshots\\111.jpg")
+        val image = loadImage(path)
         imshow("Original Image", image)
         // 创建输出单通道图像
         val grayImage = image?.apply { Mat(image.rows(), image.cols(), CvType.CV_8SC1) }
         // 进行图像色彩空间转换
         cvtColor(image, grayImage, COLOR_RGB2GRAY);
         imshow("Processed Image", grayImage);
-        imwrite("C:\\Users\\Fengzhiwei\\Pictures\\Screenshots\\222.jpg", grayImage);
-        waitKey();
+        imwrite("${d}/222.jpg", grayImage);
+        waitKey()
     }
     fun removeWatermark(){
-        val src = loadImage("C:\\Users\\Fengzhiwei\\Pictures\\Screenshots\\111.jpg")
+        val src = loadImage(path)
         val gray = Mat()
-        cvtColor(src, gray, Imgproc.COLOR_BGR2GRAY)
+        cvtColor(src, gray, COLOR_BGR2GRAY)
         val mask1 = Mat()
         val threshold = Mat()
         val edges = Mat()
         // 进行掩膜处理
-        Imgproc.medianBlur(gray, mask1, 15)
-        Imgproc.adaptiveThreshold(
+        medianBlur(gray, mask1, 15)
+        adaptiveThreshold(
             mask1,
             threshold,
             255.0,
@@ -53,17 +60,17 @@ object WatermarkUtils {
         // 定位水印区域
         val hierarchy = Mat()
         val contours: List<MatOfPoint> = ArrayList()
-        Imgproc.findContours(edges, contours, hierarchy, Imgproc.RETR_TREE, Imgproc.CHAIN_APPROX_SIMPLE)
+        findContours(edges, contours, hierarchy, Imgproc.RETR_TREE, Imgproc.CHAIN_APPROX_SIMPLE)
         // 标识定位到的水印区域
         for (i in contours.indices) {
-            Imgproc.drawContours(src, contours, i, Scalar(0.0, 0.0, 255.0), 2)
+            drawContours(src, contours, i, Scalar(0.0, 0.0, 255.0), 2)
         }
         imshow("Contours", src)
         waitKey()
         // 将水印区域覆盖为周围区域的平均像素值
         for (i in contours.indices) {
             val mask: Mat = Mat.zeros(edges.size(), CvType.CV_8UC1)
-            Imgproc.drawContours(mask, contours, i, Scalar(255.0, 255.0, 255.0), -1)
+            drawContours(mask, contours, i, Scalar(255.0, 255.0, 255.0), -1)
             val mean = Mat()
             Core.mean(src, mask)
             if (src != null) {
@@ -81,26 +88,70 @@ object WatermarkUtils {
             }
         }
     }
-    fun coverMean() {
-        // 读取带有水印的图片
-        val s = "C:\\Users\\Fengzhiwei\\Pictures\\Screenshots\\111.jpg"
-        val image = loadImage(s)
-        // 创建蒙版
-        val mask = Mat()
-        threshold(image, mask, 200.0, 255.0, THRESH_BINARY_INV)
-        // 去除水印
-        val result = Mat()
+    fun watermark(){
+        val image = Imgcodecs.imread(path)
+        // 水印区域
+        val watermarkRect = Rect(50, 50, 200, 100)
+        // 获取水印区域的均值
+        val watermarkMean = Core.mean(image.submat(watermarkRect))
+        // 替换水印区域的像素值
+        for (i in watermarkRect.y until watermarkRect.y + watermarkRect.height) {
+            for (j in watermarkRect.x until watermarkRect.x + watermarkRect.width) {
+                val pixel = image[i, j]
+                // 如果当前像素属于水印区域，将其像素值修改为均值
+                if (watermarkRect.contains(Point(j.toDouble(), i.toDouble()))) {
+                    pixel[0] = watermarkMean.`val`[0]
+                    pixel[1] = watermarkMean.`val`[1]
+                    pixel[2] = watermarkMean.`val`[2]
+                    image.put(i, j, pixel[0], pixel[1], pixel[2])
+                }
+            }
+        }
+        // 显示修改后的图像
+        imwrite("${d}/watermark_removed.jpg", image)
+    }
+    fun remove(){
+        val img = Imgcodecs.imread(path)
+        // 先灰度化下
+        val gray = Mat()
+        cvtColor(img, gray, COLOR_BGRA2GRAY)
+        // 高斯模糊
+        GaussianBlur(gray, gray, Size(3.0, 3.0), 0.0)
+        val height = img.height()
+        val width = img.width()
+        // 图片二值化处理，把[240, 240, 240] ~ [255, 255, 255]以外的颜色变成0
+        val thresh = Mat()
+        // inRange(img, new Scalar(240, 240, 240), new Scalar(255, 255, 255), thresh);
+        // 二值化处理 ,两种方式都可以实现二值化操作
+        threshold(gray, thresh, 0.0, 255.0, THRESH_BINARY or THRESH_OTSU)
+        // 创建形状和尺寸的结构元素
+        val hiMask = Mat()
+        val kernel = getStructuringElement(MORPH_RECT, Size(3.0, 3.0))
+        // 对Mask膨胀处理, 扩张待修复区域
+        dilate(thresh, hiMask, kernel)
+        // 图像修复
+        val specular = Mat()
+        Photo.inpaint(img, hiMask, specular, 5.0, Photo.INPAINT_TELEA)
+        // 显示原图
+        HighGui.namedWindow("Image", 0)
+        HighGui.resizeWindow("Image", width / 2, height / 2)
+        imshow("Image", img)
+        // 显示修复后的图
+        HighGui.namedWindow("newImage", 0)
+        HighGui.resizeWindow("newImage", width / 2, height / 2)
+        imshow("newImage", specular)
 
-        // 保存结果
-        imwrite("C:\\Users\\Fengzhiwei\\Pictures\\Screenshots\\result.jpg", result)
+        waitKey()
+        // 释放所有的窗体资源
+        // HighGui.destroyAllWindows()
     }
 }
 
 fun main(){
-    OpenCV.loadShared();
-    val s = "C:\\Users\\Fengzhiwei\\Pictures\\Screenshots\\111.jpg"
-    // WatermarkUtils.saveImage(loadImage(s), "C:\\Users\\Fengzhiwei\\Pictures\\Screenshots\\222.jpg")
+    // OpenCV .......
+    OpenCV.loadShared()
     // WatermarkUtils.testOpenCV()
-    WatermarkUtils.removeWatermark()
-    println()
+    // WatermarkUtils.removeWatermark()
+    // WatermarkUtils.watermark()
+    WatermarkUtils.remove()
 }
